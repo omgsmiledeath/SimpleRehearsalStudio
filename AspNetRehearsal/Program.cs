ba8d16e47596ec0b360f786740bf228996058018
Holiday <araragikyn@gmail.com>
2023-06-08 13:14:11 +0800

    default route

--- AspNetRehearsal/Program.cs
+++ AspNetRehearsal/Program.cs
@@ -1,7 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Studio}/{action=Index}/{id?}",
+    defaults: new {controller = "Studio" , action = "Index"});
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
 
 app.Run();
